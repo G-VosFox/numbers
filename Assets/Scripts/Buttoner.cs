809d63efa4aa07bf1103ff22bf915caaea2ec5bd
G-VosFox <144125620+G-VosFox@users.noreply.github.com>
2024-01-10 12:43:34 +0100

    got a spawner thingie

---
+++ Assets/Scripts/Buttoner.cs
@@ -0,0 +1,73 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class buttoner : MonoBehaviour
+{
+    public GameObject number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    Vector2 place = new Vector2(7, 2);
+    public void Button1()
+    {
+        number.GetComponent<Drag>().value = 1;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button2()
+    {
+        number.GetComponent<Drag>().value = 2;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button3()
+    {
+        number.GetComponent<Drag>().value = 3;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button4()
+    {
+        number.GetComponent<Drag>().value = 4;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button5()
+    {
+        number.GetComponent<Drag>().value = 5;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button6()
+    {
+        number.GetComponent<Drag>().value = 6;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button7()
+    {
+        number.GetComponent<Drag>().value = 7;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button8()
+    {
+        number.GetComponent<Drag>().value = 8;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button9()
+    {
+        number.GetComponent<Drag>().value = 9;
+        Instantiate(number, place, Quaternion.identity);
+    }
+    public void Button0()
+    {
+        number.GetComponent<Drag>().value = 0;
+        Instantiate(number, place, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -1,19 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Drag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int value = 1;
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
+    public Sprite sprite4;
+    public Sprite sprite5;
+    public Sprite sprite6;
+    public Sprite sprite7;
+    public Sprite sprite8;
+    public Sprite sprite9;
+    public Sprite sprite0;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        if (value == 1)
+        {
+            spriteRenderer.sprite = sprite1;
+        }
+        else if (value == 2)
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+        else if (value == 3)
+        {
+            spriteRenderer.sprite = sprite3;
+        }
+        else if (value == 4)
+        {
+            spriteRenderer.sprite = sprite4;
+        }
+        else if (value == 5)
+        {
+            spriteRenderer.sprite = sprite5;
+        }
+        else if (value == 6)
+        {
+            spriteRenderer.sprite = sprite6;
+        }
+        else if (value == 7)
+        {
+            spriteRenderer.sprite = sprite7;
+        }
+        else if (value == 8)
+        {
+            spriteRenderer.sprite = sprite8;
+        }
+        else if (value == 9)
+        {
+            spriteRenderer.sprite = sprite9;
+        }
+        else if (value == 0)
+        {
+            spriteRenderer.sprite = sprite0;
+        }
+    }
+    void OnMouseDrag()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mousePosition;
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("yas");
+        if (collision.gameObject.CompareTag("Number"))
+        {
+            Debug.Log("yhas queen");
+
+            Vector2 midPoint = (transform.position + collision.transform.position) / 2;//position of new num
+        }
+    }
 }
+
