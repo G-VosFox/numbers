0617d69c8c83a12fb12ed1b1a9974a9bc6eaefac
G-VosFox <144125620+G-VosFox@users.noreply.github.com>
2024-01-15 11:50:37 +0100

    shows numbers above nine

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -4,69 +4,20 @@ using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Drag : MonoBehaviour
 {
     public int value = 1;
     public int newValue;
-    public SpriteRenderer spriteRenderer;
-    public Sprite sprite1;
-    public Sprite sprite2;
-    public Sprite sprite3;
-    public Sprite sprite4;
-    public Sprite sprite5;
-    public Sprite sprite6;
-    public Sprite sprite7;
-    public Sprite sprite8;
-    public Sprite sprite9;
-    public Sprite sprite0;
     Vector3 screenPoint;
     Vector3 offset;
+    public TMP_Text showValue;
     public GameObject number;
 
     private void Update()
     {
-
-        if (value == 1)//to get the corect number on the sprite(prob ganna change later)
-        {
-            spriteRenderer.sprite = sprite1;
-        }
-        else if (value == 2)
-        {
-            spriteRenderer.sprite = sprite2;
-        }
-        else if (value == 3)
-        {
-            spriteRenderer.sprite = sprite3;
-        }
-        else if (value == 4)
-        {
-            spriteRenderer.sprite = sprite4;
-        }
-        else if (value == 5)
-        {
-            spriteRenderer.sprite = sprite5;
-        }
-        else if (value == 6)
-        {
-            spriteRenderer.sprite = sprite6;
-        }
-        else if (value == 7)
-        {
-            spriteRenderer.sprite = sprite7;
-        }
-        else if (value == 8)
-        {
-            spriteRenderer.sprite = sprite8;
-        }
-        else if (value == 9)
-        {
-            spriteRenderer.sprite = sprite9;
-        }
-        else if (value == 0)
-        {
-            spriteRenderer.sprite = sprite0;
-        }
+        showValue.text = "" + value;
     }
     void OnMouseDown()
     {
